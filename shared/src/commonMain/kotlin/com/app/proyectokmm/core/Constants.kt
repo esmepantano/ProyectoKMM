3fbb93613ec9b6d561e9fd52bbade7dfaa10866c
esmepantano <pantanoesmeralda@gmail.com>
2025-06-24 00:42:33 -0300

    keys api

--- shared/src/commonMain/kotlin/com/app/proyectokmm/core/Constants.kt
+++ shared/src/commonMain/kotlin/com/app/proyectokmm/core/Constants.kt
@@ -1,4 +1,4 @@
 package com.app.proyectokmm.core
 
-internal val PUBLIC_KEY = "your_public_key"
-internal val PRIVATE_KEY = "your_private_key"
+internal val PUBLIC_KEY = "364aeda5208d34ba5d301567144ee6a9"
+internal val PRIVATE_KEY = "40a6d9bae3800f5d55bd1ca457b00d298c42c0b4"
